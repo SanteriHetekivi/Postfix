b7ec9ec91710591bd5151496a7cc798fd9282df7
Santeri Hetekivi <santeri@hetekivi.com>
2015-10-14 13:24:06 +0300

    Start

---
+++ Postfix.cpp
@@ -0,0 +1,20 @@
+/*
+	Santeri Hetekivi (1202466)
+	1. harjoitustyö (Postfix-laskin)
+	Tietorakenteet ja algoritmit (5G00BD79-3001)
+	Tampere University of Applied Sciences (13I224)
+	10/2015
+*/
+
+#include "Postfix.hpp"
+
+
+
+Postfix::Postfix()
+{
+}
+
+
+Postfix::~Postfix()
+{
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+	Santeri Hetekivi (1202466)
+	1. harjoitustyö (Postfix-laskin)
+	Tietorakenteet ja algoritmit (5G00BD79-3001)
+	Tampere University of Applied Sciences (13I224)
+	10/2015
+*/
+
+int main()
+{
+	return 1;
+}
